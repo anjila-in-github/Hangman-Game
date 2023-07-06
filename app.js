5ac37fa6fdc068d581e162e65a647dee537a477f
Dikshya <136353386+dikshya-in-github@users.noreply.github.com>
2023-07-06 08:41:56 +0545

    Add files via upload

--- app.js
+++ app.js
@@ -23,13 +23,13 @@ particlesJS('particles-js',
         }
       },
       "color": {
-        "value": "#141E30"
+        "value": "#1F6E8C"
       },
       "shape": {
         "type": "circle",
         "stroke": {
-          "width": 0,
-          "color": "#000000"
+          "width": 1,
+          "color": "#0E2954"
         },
         "polygon": {
           "nb_sides": 5
@@ -63,7 +63,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#ffffff",
+        "color": "#FDCEDF",
         "opacity": 0.4,
         "width": 1
       },
