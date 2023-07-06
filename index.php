15367a02da23f09cb0972c5577966ffd3015c242
ANJILA <136354887+anjila-in-github@users.noreply.github.com>
2023-07-06 16:10:33 +0545

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+   <style>
+    .gff{
+        font-family:"century gothic";
+        font-size:40px;
+        color:#fff;
+        position:absolute;
+        top:200px;
+        left:800px;
+    
+    }
+    </style>
+    <body style="background:url(hpp.jpg);background-repeat:no-repeat;background-size;100% 100%">
+    <div class="gff"
+    <center>
+        <a href="category.php">SELECT CATEGORY</a>
+</center>
+</html> 
