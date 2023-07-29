f516a7da13656df5688e96389525eb94660e45a8
anjila089 <anjilas241@gmail.com>
2023-07-29 16:01:27 +0545

    feat:code for score

--- score.php
+++ score.php
@@ -1,13 +1,19 @@
 <?php
+
+require 'connect.php';
+
 $db_host="localhost";
 $db_user="root";
 $db_password="";
 
-$lnk=mysqli_connet($db_host,$db_user,db_password);
-if(!lnk)
+$lnk= mysqli_connect($db_host,$db_user,$db_password);
+if(!$lnk)
+
     die("Database connection failed");
 
     mysqli_select_db($lnk,"hangman") or die("falied to connect the db");
+
+    
 
     $result=getAllScores($lnk);
     echo json_encode($result);
@@ -16,7 +22,7 @@ if(!lnk)
         $query="Insert INTO scores(Name ,Password) VALUES". 
         "('".$info["name"]."')";
         $rs=mysqli_query($lnk,$query);
-        if (!rs){
+        if (!$rs){
             return false;
 
         }
@@ -26,15 +32,14 @@ if(!lnk)
     $easy=getScoresWithDifficulty("Easy",$lnk);
 
     $medium=getScoresWithDifficulty("medium",$lnk);
-    return array("easy"=>$easy,"medium"=>medium);
+    return array("easy"=>$easy,"medium"=>$medium);
     
     }
 
     function getScoresWithDifficulty($difficulty,$lnk){
-    $query="Select Name from score where difficulty like 'easy'";
-
-    $rs=mysqli_query($lnk,$query);
-
+        $query="Select score from hangman where username=username'";
+        $rs=mysqli_query($lnk,$query);
+        
     $result=array();
     if(mysqli_num_rows($rs)>0){
         while ($row = mysqli_fetch_assoc($rs)){
