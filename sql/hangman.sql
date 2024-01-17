3dbaa4a6bedb56f1db9c470863f8dcfdfb55cc96
HEY-8 <145573634+HEY-8@users.noreply.github.com>
2024-01-17 21:50:55 +0545

    add : hangman.sql

--- sql/hangman.sql
+++ sql/hangman.sql
@@ -1,50 +1,56 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 5.5.62, for Win64 (AMD64)
 --
--- Host: localhost:3306
--- Generation Time: Jul 29, 2023 at 09:58 AM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: hangman
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.32-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `hangman`
---
-
--- --------------------------------------------------------
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `login_data`
 --
 
+DROP TABLE IF EXISTS `login_data`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `login_data` (
   `username` varchar(30) NOT NULL,
-<<<<<<< HEAD
-  `password` varchar(20) NOT NULL,/*
-  `score` int(30) NOT NULL*/
-=======
-  `pwd` varchar(20) NOT NULL
->>>>>>> 983ccfc351406969210e5153c93f80f8f95f4c69
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `email` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `role` int(10) unsigned DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `login_data`
 --
 
-INSERT INTO `login_data` (`username`, `pwd`) VALUES
-('1', '11');
-COMMIT;
+LOCK TABLES `login_data` WRITE;
+/*!40000 ALTER TABLE `login_data` DISABLE KEYS */;
+INSERT INTO `login_data` VALUES ('test','test@test.com','$2y$10$qk3BK3UgbV8qk',1),('test1','test1@test.co','$2y$10$pNh5UbO0scDGB',1);
+/*!40000 ALTER TABLE `login_data` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Dumping routines for database 'hangman'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-01-17 21:49:13
